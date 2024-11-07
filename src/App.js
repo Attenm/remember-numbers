a3c63c70da463a914f25c2bbac7e653d5e2c9993
Artem Nazarchenko <tiymanazar62@gmail.com>
2024-11-08 00:26:28 +0200

    created a menu page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.scss';
 // import AddNumbersPage from './pages/AddNumbersPage/AddNumbersPage.js';
 // import ContactsPage from './pages/ContactsPage/ContactsPage.js';
-import NoContactsPage from './pages/NoContactsPage/NoContactsPage.js';
+// import NoContactsPage from './pages/NoContactsPage/NoContactsPage.js';
+import MenuPage from './pages/MenuPage/MenuPage.js';
+
 
 function App() {
   return (
     <div className="App">
       {/* <AddNumbersPage/> */}
       {/* <ContactsPage /> */}
-      <NoContactsPage />
+      {/* <NoContactsPage /> */}
+      <MenuPage/>
     </div>
   );
 }
---
+++ src/pages/MenuPage/MenuPage.js
@@ -0,0 +1,18 @@
+import './menuPage.scss';
+
+function MenuPage() {
+    return (
+        <div className="menu__wrapper">
+            <ul className='menu__list'>
+                <li className='menu__item'>
+                    My contacts
+                </li>
+                <li className='menu__item'>
+                    Play
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default MenuPage;
